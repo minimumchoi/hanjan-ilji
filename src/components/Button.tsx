e20e55d093a432cb959f74fc548c682d20b436ca
채소 <chlthgus0901@naver.com>
2025-04-10 11:49:00 +0900

    feat : Button 컴포넌트 생성

---
+++ src/components/Button.tsx
@@ -0,0 +1,47 @@
+import { ReactNode } from "react";
+
+type ButtonType = {
+  size?: "s" | "m" | "l";
+  color?: "primary" | "accent";
+  children: ReactNode;
+  onClick: () => void;
+  disabled: boolean;
+};
+
+export default function Button({
+  size = "l",
+  color = "primary",
+  children,
+  onClick,
+  disabled = false,
+}: ButtonType) {
+  const BtnSize =
+    size === "l"
+      ? "h-20 w-86 text-2xl"
+      : size === "m"
+        ? "h-15 w-54 text-xl"
+        : "h-15 w-43 text-xl";
+
+  const BtnColor = (() => {
+    if (disabled) {
+      return color === "primary"
+        ? "bg-primary/40 text-white/70 cursor-not-allowed"
+        : "bg-accent/50 text-text/60 cursor-not-allowed";
+    }
+
+    return color === "primary"
+      ? "bg-primary text-white cursor-pointer"
+      : "bg-accent text-text cursor-pointer";
+  })();
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`${BtnSize} ${BtnColor} rounded-2xl`}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
