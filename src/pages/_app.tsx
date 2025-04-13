a2ef6144cd322d52e09e8b2050e03da3b7a86eef
채소 <chlthgus0901@naver.com>
2025-04-13 23:04:02 +0900

    style : pretendard 폰트 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,11 @@
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
 import Layout from "@/pages/layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const noNavPages = ["/", "/login", "/signup"];
-  const isNavHidden = noNavPages.includes(router.pathname);
-
-  const content = isNavHidden ? (
-    <Component {...pageProps} />
-  ) : (
+  return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
-
-  return content;
 }
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,15 +1,39 @@
 import Navigation from "@/components/Navigation";
 import type { ReactNode } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import localFont from "next/font/local";
+
+const pretendard = localFont({
+  src: "../fonts/PretendardVariable.woff2",
+  variable: "--font-pretendard",
+  display: "swap",
+});
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 export default function Layout({ children }: LayoutProps) {
+  const router = useRouter();
+  const hideNavRoutes = ["/", "/login", "/signup"];
+  const shouldHideNav = hideNavRoutes.includes(router.pathname);
+
   return (
-    <div className="relative min-h-screen">
-      <main className="pb-[3.75rem]">{children}</main>
-      <Navigation />
-    </div>
+    <>
+      <Head>
+        <title>한잔일지</title>
+        <meta name="description" content="적당히 즐기는 음주 습관 기록장" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        className={`${pretendard.variable} font-pretendard relative min-h-screen`}
+      >
+        <main className={shouldHideNav ? "" : "pb-[3.75rem]"}>{children}</main>
+        {!shouldHideNav && <Navigation />}
+      </div>
+    </>
   );
 }
