f633dbb3cd27b0d9a891edf45b61ab636858685d
채소 <chlthgus0901@naver.com>
2025-04-14 17:17:08 +0900

    feat : DropDown, DropDownList 컴포넌트 구현

---
+++ src/components/DropDown.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { SVGIcon } from "./SVGIcon";
+import DropDownList from "./DropDownList";
+
+type DropBoxProp = {
+  width: "sm" | "lg";
+  title?: string;
+  listArr: string[];
+};
+
+export default function DropDown({
+  width,
+  title = "선택해주세요",
+  listArr,
+}: DropBoxProp) {
+  const [isOpened, setIsOpened] = useState(false);
+  const [name, setName] = useState(title || "선택해주세요");
+
+  const widthSize = {
+    lg: "w-54",
+    sm: "w-23",
+  }[width];
+
+  const textColor = name === "선택해주세요" ? "text-gray-300" : "text-black";
+
+  const onClick = (e: string) => {
+    setName(e);
+    setIsOpened(false);
+  };
+
+  return (
+    <div className="relative inline-block">
+      <button
+        type="button"
+        aria-haspopup="listbox"
+        aria-expanded={isOpened}
+        className={`${widthSize} flex h-12 cursor-pointer flex-row items-center rounded-xl border-3 ${isOpened ? "border-primary" : "border-gray-300"} px-2 py-[6px]`}
+        onClick={() => setIsOpened(!isOpened)}
+      >
+        <span className={`${textColor} flex-1 text-base font-bold`}>
+          {name}
+        </span>
+
+        <SVGIcon
+          name="arrow"
+          size={23}
+          className={`text-black transition-transform duration-300 ease-in-out ${
+            isOpened ? "text-primary rotate-180" : ""
+          }`}
+        />
+      </button>
+      {isOpened && (
+        <DropDownList
+          selected={name}
+          listArr={listArr}
+          width={widthSize}
+          onClick={onClick}
+        />
+      )}
+    </div>
+  );
+}
---
+++ src/components/DropDownList.tsx
@@ -0,0 +1,32 @@
+type DropBoxListProp = {
+  listArr: string[];
+  width: string;
+  onClick: (value: string) => void;
+  selected: string;
+};
+
+export default function DropDownList({
+  listArr,
+  width,
+  onClick,
+  selected,
+}: DropBoxListProp) {
+  return (
+    <ul
+      role="listbox"
+      className={`${width} border-primary absolute top-13 z-10 mt-[3px] overflow-hidden rounded-xl border-3 bg-white shadow-md`}
+    >
+      {listArr.map((list) => (
+        <li key={list} role="option" aria-selected={selected === list}>
+          <button
+            type="button"
+            className={`hover:text-primary flex h-10 w-full items-center justify-center text-base font-semibold text-black transition-colors duration-150 hover:bg-gray-100`}
+            onClick={() => onClick(list)}
+          >
+            {list}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
