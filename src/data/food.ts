764bc2c5de358c5f91330d632a17621a9df2a2a4
채소 <chlthgus0901@naver.com>
2025-05-09 22:24:09 +0900

    feat: 안주 추천용 데이터 추가

---
+++ src/data/food.ts
@@ -0,0 +1,107 @@
+export type ruletFoodProp = {
+  name: string;
+  description: string;
+};
+
+export const ruletFood: ruletFoodProp[] = [
+  { name: "치킨", description: "맥주와 찰떡궁합, 바삭하고 고소한 국민 안주" },
+  { name: "삼겹살", description: "소주엔 역시 구운 삼겹살! 씹을수록 깊은 맛" },
+  {
+    name: "감자튀김",
+    description: "바삭한 감자에 케찹 한 스푼, 맥주랑 완벽 매칭",
+  },
+  {
+    name: "두부김치",
+    description: "따뜻한 두부에 매콤한 김치, 전통 혼술 안주",
+  },
+  {
+    name: "골뱅이무침",
+    description: "새콤달콤 골뱅이와 소면, 입맛 확 살려주는 안주",
+  },
+  {
+    name: "오징어볶음",
+    description: "매콤한 양념과 쫄깃한 식감으로 스트레스 타파",
+  },
+  {
+    name: "나쵸 & 치즈",
+    description: "살사소스와 함께 즐기는 멕시칸 스타일 안주",
+  },
+  {
+    name: "치즈플래터",
+    description: "와인과 완벽한 조화, 고급스러운 맛의 향연",
+  },
+  {
+    name: "계란말이",
+    description: "부드럽고 고소한 기본 안주, 해장 느낌도 있음",
+  },
+  { name: "쥐포구이", description: "불맛 가득한 옛날 감성 간단 안주" },
+  { name: "훈제연어", description: "깔끔하고 고급진 안주, 와인과 찰떡" },
+  { name: "닭발", description: "맵고 쫄깃한 맛으로 소주를 부르는 마성의 안주" },
+  { name: "피자", description: "맥주랑 언제나 함께하는 치즈 폭탄" },
+  {
+    name: "치즈스틱",
+    description: "바삭한 겉과 고소한 속, 맥주 안주계 귀염둥이",
+  },
+  { name: "오징어튀김", description: "겉은 바삭, 속은 쫄깃한 바다의 풍미" },
+  { name: "버팔로윙", description: "매콤짭짤한 닭날개, 맥주와 찰떡궁합" },
+  { name: "탕수육", description: "부먹파 찍먹파 다 환영! 바삭함과 단짠 조합" },
+  { name: "족발", description: "부드러운 식감에 새우젓 한 스푼이면 소주 끝" },
+  { name: "보쌈", description: "김치랑 한 쌈 싸먹으면 꿀맛, 회식 안주 대표" },
+  {
+    name: "간장계란밥",
+    description: "은근히 중독성 있는 혼술용 간단 식사 안주",
+  },
+  { name: "떡볶이", description: "맵단맵단! 소주와도 맥주와도 잘 어울림" },
+  { name: "비빔면", description: "차가운 매콤함으로 입맛과 술맛 둘 다 잡기" },
+  {
+    name: "건어물 세트",
+    description: "쥐포, 먹태, 오징어, 간단하게 술안주 완성",
+  },
+  {
+    name: "스테이크 슬라이스",
+    description: "위스키와 어울리는 고급 고기 안주",
+  },
+  {
+    name: "마라탕",
+    description: "마라향이 입안을 지배할 때 시원한 맥주 한 잔",
+  },
+  { name: "라면", description: "국물 있는 안주로 혼술 끝판왕" },
+  { name: "치즈라면", description: "국물에 치즈 풀리면 맵단조합 끝판왕" },
+  {
+    name: "치즈볼",
+    description: "쫀득달콤한 한 입 간식, 달달한 술과 잘 어울림",
+  },
+  {
+    name: "연어사시미",
+    description: "부드럽고 고소한 생선, 소주·와인 모두 환영",
+  },
+  {
+    name: "홍합탕",
+    description: "따끈한 국물에 시원한 술 한 잔, 해장용으로도 굿",
+  },
+  { name: "문어숙회", description: "쫄깃한 식감과 깔끔한 술맛의 조화" },
+  {
+    name: "낙지볶음",
+    description: "매콤하고 부드러운 바다맛, 소주를 부르는 맛",
+  },
+  {
+    name: "카프레제",
+    description: "모짜렐라 + 토마토 + 바질로 상큼한 와인 안주",
+  },
+  { name: "바게트 & 오일", description: "심플하지만 고급스러운 와인 곁들임" },
+  {
+    name: "과일 플래터",
+    description: "달콤하고 산뜻하게, 와인/위스키 둘 다 OK",
+  },
+  { name: "초콜릿", description: "특히 위스키와 어울리는 쌉쌀한 디저트 안주" },
+  { name: "견과류", description: "술맛 방해 안 하고 계속 손이 가는 스낵 안주" },
+  {
+    name: "비프저키",
+    description: "짭짤하고 오래 씹는 고기 안주, 위스키와 찰떡",
+  },
+  {
+    name: "에어프라이어 감자",
+    description: "간편하게 만드는 포실포실 감튀 버전",
+  },
+  { name: "핫바", description: "편의점 혼술용 든든하고 짭조름한 한입 안주" },
+];
