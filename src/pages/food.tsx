1fe71329349d0e0f5d8fbbd9dcf05eec82d1a93e
채소 <chlthgus0901@naver.com>
2025-05-18 00:13:47 +0900

    feat : food 페이지 기초 구성

--- src/pages/food.tsx
+++ src/pages/food.tsx
@@ -1,3 +1,19 @@
+import Button from "@/components/Button";
+import Roulette from "@/components/Roulette";
+import RuletButton from "@/components/RuletButton";
+
 export default function Food() {
-  return;
+  const handleRuletClick = () => {};
+  return (
+    <div className="flex flex-col items-center gap-5">
+      <div className="mt-24 flex flex-col items-center">
+        <h1 className="text-2xl font-bold">오늘 뭐 먹지?</h1>
+        <span className="text-sm font-bold">
+          다양한 안주를 랜덤으로 추천해드려요 🎲
+        </span>
+      </div>
+      <Roulette />
+      <RuletButton />
+    </div>
+  );
 }
