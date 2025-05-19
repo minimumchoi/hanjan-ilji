75a63df0b0ae5f8f9b01a13c5a048dee347fd565
채소 <chlthgus0901@naver.com>
2025-05-19 22:22:19 +0900

    feat : Input 컴포넌트 수정
    - validMsg 수정
    - input type 추가
    - pattern 제거
    - validation 메세지 스타일링 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ type InputProp = {
   type: InputType;
   showValidation: boolean;
   value: string;
-  onChange: () => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   labelId: string;
 };
 
@@ -18,19 +18,20 @@ export default function Input({
   const config = {
     email: {
       placeholder: "email@example.com",
-      validMsg: "이메일을 입력해주세요",
+      validMsg: "올바른 이메일 형식이 아닙니다.",
     },
     password: {
       placeholder: "비밀번호를 입력해주세요",
-      validMsg: "비밀번호를 확인해주세요",
+      validMsg:
+        "영문 대소문자, 숫자, 특수문자를 포함해 10자 이상 입력해주세요.",
     },
     passwordCheck: {
       placeholder: "비밀번호를 입력해주세요",
-      validMsg: "비밀번호를 확인해주세요",
+      validMsg: "비밀번호가 일치하지 않습니다.",
     },
     nickName: {
       placeholder: "닉네임을 입력해주세요",
-      validMsg: "닉네임을 확인해주세요",
+      validMsg: "닉네임은 10자 이내로 입력해주세요.",
     },
   };
   const { placeholder, validMsg } = config[type];
@@ -38,10 +39,13 @@ export default function Input({
   return (
     <>
       <input
-        type={type === "email" ? "email" : "text"}
-        {...(type === "email"
-          ? { pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$" }
-          : {})}
+        type={
+          type === "email"
+            ? "email"
+            : type === "password" || "passwordCheck"
+              ? "password"
+              : "text"
+        }
         className="focus:border-primary h-12 w-full border-b-3 border-gray-300 p-1.5 text-lg outline-none placeholder:text-base placeholder:text-gray-300"
         placeholder={placeholder}
         value={value}
@@ -49,11 +53,11 @@ export default function Input({
         id={labelId}
       />
       {showValidation ? (
-        <span className="mt-1 h-6 px-1.5 text-base text-red-500">
+        <span className="mt-1 min-h-6 p-1.5 text-sm text-red-500">
           {validMsg}
         </span>
       ) : (
-        <span className="mt-1 h-6 px-1.5 text-base"></span>
+        <span className="mt-1 min-h-6 p-1.5 text-sm"></span>
       )}
     </>
   );
