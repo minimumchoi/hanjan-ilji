d24b00645f38922bcec80b384e625d8597b6e9bd
채소 <chlthgus0901@naver.com>
2025-05-21 18:05:51 +0900

    modify : prop추가

--- src/components/DrinkInput.tsx
+++ src/components/DrinkInput.tsx
@@ -1,35 +1,39 @@
+import { RefObject } from "react";
+
 type DrinkInputProp = {
   type: "text" | "number";
-  showValidation: boolean;
+  value: number | string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  ref?: RefObject<HTMLDialogElement | null>;
 };
 export default function DrinkInput({
   type,
-  showValidation = false,
+  onChange,
+  value,
+  ref,
 }: DrinkInputProp) {
   const config = {
     text: {
       placeHolder: "입력해주세요",
       size: "w-54",
-      validMsg: "입력해주세요",
     },
     number: {
       placeHolder: "0",
       size: "w-15",
-      validMsg: "입력해주세요",
     },
   };
 
-  const { placeHolder, size, validMsg } = config[type];
+  const { placeHolder, size } = config[type];
   return (
     <>
       <input
+        ref={ref}
         type={type}
         className={`focus:border-primary h-12 text-center ${size} border-b-3 border-gray-300 p-1.5 text-lg outline-none placeholder:text-center placeholder:text-lg placeholder:text-gray-300`}
         placeholder={placeHolder}
+        value={value}
+        onChange={onChange}
       ></input>
-      {showValidation && (
-        <span className="mt-1 px-1.5 text-base text-red-500">{validMsg}</span>
-      )}
     </>
   );
 }
