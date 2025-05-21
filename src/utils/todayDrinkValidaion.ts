0bed054104fe896b7e7d2227448323ddc4f17092
채소 <chlthgus0901@naver.com>
2025-05-21 18:12:58 +0900

    feat : 오늘의 한잔 유효성 검사 기능 및 직접입력 기능 구현
    - DropDown으로 직접입력 선택시 DrinkInput 표시
    - todayDrinkValidation 통해 유효성 검사 및 경고 메세지 표시
    - 유효성 검사 후 에러가 있다면 제출 x

---
+++ src/utils/todayDrinkValidaion.ts
@@ -0,0 +1,40 @@
+export const isValidDrinkType = (
+  drinkType: string,
+  customDrinkType: string,
+) => {
+  if (typeof drinkType !== "string" || drinkType.trim() === "") {
+    return "마신 술 종류를 선택해주세요";
+  }
+  if (
+    drinkType === "직접입력" &&
+    (!customDrinkType || customDrinkType.trim() === "")
+  ) {
+    return "직접 입력한 술 이름이 비어 있어요";
+  }
+  return;
+};
+
+export const isValidAmount = (amount: string) => {
+  if (typeof amount !== "string" || amount.trim() === "") {
+    return "마신 양을 입력해주세요";
+  }
+  const numericAmount = Number(amount);
+  if (isNaN(numericAmount) || numericAmount <= 0) {
+    return "1 이상으로 입력해주세요";
+  }
+  return;
+};
+
+export const isValidWithWhom = (withWhom: string) => {
+  if (typeof withWhom !== "string" || withWhom.trim() === "") {
+    return "혼자 마셨다면 '혼자'라고 입력해 주세요";
+  }
+  return;
+};
+
+export const isValidFeeling = (feeling: string) => {
+  if (typeof feeling !== "string" || feeling.trim() === "") {
+    return "오늘의 기분을 선택해주세요";
+  }
+  return;
+};
