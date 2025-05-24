2876574f5c90e68a49ab43869b61989c3666c775
채소 <chlthgus0901@naver.com>
2025-05-24 22:26:52 +0900

    feat : MonthlyProgress에 전달해줄 prop 데이터 패칭
    - 해당 월에 해당하는 데이터만 필터링

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,58 @@
 import Button from "@/components/Button";
 import MonthlyProgress from "@/components/MonthlyProgress";
+
+import { createClient } from "@/utils/supabase/component";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const supabase = createClient();
   const router = useRouter();
+  const [drinkCount, setDrinkCount] = useState(0);
+
+  const getCountData = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    const now = new Date();
+    const firstDay = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      1,
+    ).toISOString();
+    const lastDay = new Date(
+      now.getFullYear(),
+      now.getMonth() + 1,
+      0,
+      23,
+      59,
+      59,
+    ).toISOString();
+
+    const { data, error } = await supabase
+      .from("dailyDrink")
+      .select()
+      .eq("user_id", user?.id)
+      .gte("created_at", firstDay)
+      .lte("created_at", lastDay);
+
+    if (error) {
+      console.log("에러");
+      return;
+    }
+    return data.length;
+  };
+
+  // 나중에 제거
+  const handleOnclick = async () => {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+  };
 
   const handleTodayDrink = () => {
     router.push("./todayDrink");
@@ -13,6 +62,15 @@ export default function Home() {
     router.push("./monthlyLimit");
   };
 
+  useEffect(() => {
+    const fetchDrinkCount = async () => {
+      const count = await getCountData();
+      setDrinkCount(count);
+    };
+
+    fetchDrinkCount();
+  });
+
   return (
     <div className="mt-19 flex w-full flex-col items-center justify-center gap-8 px-9">
       <div>
@@ -21,7 +79,7 @@ export default function Home() {
         </span>
         <h1 className="text-[2.5rem] font-bold">한잔일지 🍷</h1>
       </div>
-      <MonthlyProgress totalLimit={8} drinkCount={1} />
+      <MonthlyProgress totalLimit={8} drinkCount={drinkCount} />
       <div className="flex w-full flex-col gap-4">
         <Button color="primary" onClick={handleTodayDrink}>
           오늘의 한잔 기록하기
@@ -29,6 +87,7 @@ export default function Home() {
         <Button color="accent" onClick={handleMonthlyLimit}>
           이달의 목표 정하기
         </Button>
+        <Button onClick={handleOnclick}>로그아웃</Button>
       </div>
     </div>
   );
