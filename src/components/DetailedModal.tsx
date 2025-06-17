f8d615c68e665bb7b274dc6b5926e0b150fe93eb
채소 <chlthgus0901@naver.com>
2025-06-17 14:26:08 +0900

    feat : 편집 삭제 기능 추가 및 아이콘 ui 변경

--- src/components/DetailedModal.tsx
+++ src/components/DetailedModal.tsx
@@ -1,12 +1,16 @@
 import { DetailedModalProp, ModalListProp } from "@/types/propTypes";
 import ModalList from "./ModalList";
 import { useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import { SVGIcon } from "./SVGIcon";
 
 export default function DetailedModal({
   drinkData,
   onClose,
+  onDelete,
 }: DetailedModalProp) {
   const modalRef = useRef<HTMLDialogElement>(null);
+  const router = useRouter();
 
   const modalListData: ModalListProp[] = [
     { name: "마신 술", value: drinkData.drink },
@@ -19,7 +23,16 @@ export default function DetailedModal({
     modalRef.current?.showModal();
   }, []);
 
-  const handleEdit = () => {};
+  const handleDelete = () => {
+    onDelete(drinkData.id, () => {
+      modalRef.current?.close();
+      onClose();
+    });
+  };
+
+  const handleEdit = () => {
+    router.push(`./todayDrink/edit/${drinkData.id}`);
+  };
   const handleClose = () => {
     modalRef.current?.close();
     onClose();
@@ -31,17 +44,21 @@ export default function DetailedModal({
       className="bg-background fixed top-1/2 left-1/2 h-115 w-83 -translate-x-1/2 -translate-y-1/2 rounded-lg backdrop:bg-gray-300"
     >
       <div className="text-text flex w-full flex-col items-center justify-center">
-        <div className="mt-2.5 flex w-full flex-row items-stretch justify-between px-4.5">
+        <div className="mt-4 flex w-full flex-row items-stretch justify-between px-4.5">
           <button
             type="button"
-            className="box-border h-7 w-7 bg-purple-50 outline-none"
+            className="flex h-7 w-7 cursor-pointer flex-row items-center justify-center"
             onClick={handleClose}
-          />
+          >
+            <SVGIcon name="close" size={20} className="text-text" />
+          </button>
           <button
             type="button"
-            className="box-border h-7 w-7 bg-purple-50 outline-none"
-            onClick={handleClose}
-          />
+            className="flex h-7 w-7 cursor-pointer flex-row items-center justify-center"
+            onClick={handleDelete}
+          >
+            <SVGIcon name="trash" size={32} />
+          </button>
         </div>
         <ul className="mx-auto mt-12 flex flex-col gap-9 self-stretch">
           {modalListData.map((d) => (
@@ -56,9 +73,11 @@ export default function DetailedModal({
         </ul>
         <button
           type="button"
-          className="mt-6 h-13 w-13 rounded-full bg-gray-100 outline-none"
+          className="mt-6 flex h-13 w-13 cursor-pointer flex-row items-center justify-center rounded-full bg-gray-100 outline-none"
           onClick={handleEdit}
-        ></button>
+        >
+          <SVGIcon name="edit" size={20} />
+        </button>
       </div>
     </dialog>
   );
