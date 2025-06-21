7080bc6b924ffb896889f1206718ddea387021d5
채소 <chlthgus0901@naver.com>
2025-06-21 23:44:52 +0900

    modify : 비활성화 스타일 제거 및 스타일 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,6 @@ type ButtonProp = {
   color?: "primary" | "accent";
   children: ReactNode;
   onClick: () => void;
-  disabled?: boolean;
 };
 
 export default function Button({
@@ -13,33 +12,24 @@ export default function Button({
   color = "primary",
   children,
   onClick,
-  disabled = false,
 }: ButtonProp) {
   const BtnSize =
     size === "l"
-      ? "h-20 w-full text-2xl"
+      ? "h-18 w-full text-[1.375rem]"
       : size === "m"
         ? "h-15 w-54 text-xl"
         : "h-15 w-43 text-xl";
 
-  const BtnColor = (() => {
-    if (disabled) {
-      return color === "primary"
-        ? "bg-primary/40 text-white/70 cursor-not-allowed"
-        : "bg-accent/50 text-text/60 cursor-not-allowed";
-    }
-
-    return color === "primary"
+  const BtnColor =
+    color === "primary"
       ? "bg-primary text-white cursor-pointer"
-      : "bg-accent text-text cursor-pointer";
-  })();
+      : "bg-yellow-50 text-text cursor-pointer";
 
   return (
     <button
       type="button"
       onClick={onClick}
       className={`${BtnSize} ${BtnColor} rounded-2xl font-bold`}
-      disabled={disabled}
     >
       {children}
     </button>
