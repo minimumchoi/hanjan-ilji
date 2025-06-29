a5c7ecc0d2ad418b7daf4ba6e9f033694df07629
채소 <chlthgus0901@naver.com>
2025-06-29 12:11:55 +0900

    feat : 오늘의 한잔 기록관련 상수 분리

---
+++ src/data/drinkRecord.ts
@@ -0,0 +1,19 @@
+export const drinkArr = [
+  "소주",
+  "맥주",
+  "막걸리",
+  "위스키",
+  "와인",
+  "사케",
+  "하이볼",
+  "직접입력",
+];
+
+export const todayFeeling = [
+  "매우 좋았어요",
+  "그냥 그랬어요",
+  "스트레스를 받았어요",
+  "조금 우울했어요",
+];
+
+export const drinkUnit = ["잔", "병"];
